fe30910c1d3e68c87aeaa68b453e9eb324edc5e8
facundozto <facundozerbato@outlook.com>
2024-04-12 19:46:16 -0300

    Update css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,45 @@
-import style from './Header.module.css';
-import {useState} from 'react';
-import Web from './Web/Web';
-import Mobile from './Mobile/Mobile';
-import {motion, useViewportScroll, useTransform} from 'framer-motion';
+import style from "./Header.module.css";
+import { useState } from "react";
+import Web from "./Web/Web";
+import Mobile from "./Mobile/Mobile";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 const Header = () => {
+  const [state, setState] = useState<boolean>(false);
 
-    const [state, setState] = useState<boolean>(false);
+  //-----Cambiar background del header-----
+  const { scrollY } = useViewportScroll();
+  const background = useTransform(
+    scrollY,
+    [0.1, 1],
+    ["grey", "rgb(224, 117, 47, 0.9)"]
+  );
+  //-------------
 
-    //-----Cambiar background del header-----
-    const {scrollY} = useViewportScroll();
-    const background = useTransform(scrollY, [0.1, 1], ['transparent', 'rgb(224, 117, 47, 0.9)']);
-    //-------------
-
-    return(
-        <motion.div 
-        className={style.header}
-        style={{backgroundColor: background}}
-        >
-            <div className={style.title}>
-                Company
+  return (
+    <motion.div
+      className={style.header}
+      style={{ backgroundColor: background }}
+    >
+      <div className={style.title}>Company</div>
+      <div className={style.menu}>
+        <div className={style.web}>
+          <Web />
+        </div>
+        <div className={style.mobile}>
+          {state ? (
+            <Mobile state={state} setState={setState} />
+          ) : (
+            <div onClick={() => setState(!state)}>
+              <span className="material-symbols-outlined" id={style.mobileMenu}>
+                menu
+              </span>
             </div>
-            <div className={style.menu}>
-                <div className={style.web}>
-                    <Web/>
-                </div>
-                <div className={style.mobile}>
-                    {
-                    state ? <Mobile state={state} setState={setState} /> :
-                    <div onClick={() => setState(!state)}>
-                        <span className="material-symbols-outlined" id={style.mobileMenu}>menu</span>
-                    </div>
-                    }
-                </div>
-            </div>
-        </motion.div>
-    )
-}
+          )}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Header;
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,4 +3,7 @@ declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.png";
 declare module "*.svg";
-declare module 'react-scroll';
+declare module "react-scroll";
+declare module "react/jsx-runtime" {
+  export default any;
+}
